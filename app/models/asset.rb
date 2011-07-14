cf835e2febc44969f636726353fce08dc72c969b
ashish upadhyay <puneet_pandey85@yahoo.co.in>
2011-07-14 14:22:34 +0530

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/models/asset.rb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/users/_form.html.erb 	modified:   app/views/users/index.html.erb 	modified:   config/database.yml 	new file:   nbproject/private/rake-d.txt 	modified:   public/javascripts/rails.js 	new file:   public/system/assets/1/large/Lighthouse.jpg 	new file:   public/system/assets/1/medium/Lighthouse.jpg 	new file:   public/system/assets/1/original/Lighthouse.jpg 	new file:   public/system/assets/1/thumb/Lighthouse.jpg 	new file:   public/system/assets/2/large/Koala.jpg 	new file:   public/system/assets/2/medium/Koala.jpg 	new file:   public/system/assets/2/original/Koala.jpg 	new file:   public/system/assets/2/thumb/Koala.jpg

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
   belongs_to  :user
-has_attached_file  :asset, :styles => {  :large => "800*600>",  :medium => "150*150>",  :thumb => "150*150>" }
+has_attached_file  :asset, :styles => {  :large => "800*600>",  :medium => "150*150>",  :thumb => "50*50>" }
 
 end
